64dad92f3753b5edb239128fe9342e905bcc9252
Filip Burlacu <filip.burlacu@securekey.com>
2019-09-18 17:39:20 -0400

    Successful tests with data generated in aries-framework-go

--- aries_cloudagent/box_seal_test.py
+++ aries_cloudagent/box_seal_test.py
@@ -1,18 +1,36 @@
 #!/usr/bin/env python3
 
 from .wallet import crypto
-from .wallet.util import b64_to_bytes
+from .wallet.util import b64_to_bytes, b58_to_bytes, bytes_to_b58
 import nacl.bindings
 
-# Paste in b64url data
-b64_pub = "cy_nKX-bPHahdPNHGFjhXX0Jm4JepTIPK0e21vYAo30="
-b64_priv = "_v_aZ_yl8vbwftKEasInlnBkendrF8wEgklHyOV5UvE="
-msg_in = "T8ZNSXLmIdSqHIp8EvS6bXci7tDru_7YoGCOeWJAOtFhnsQgETbMg6sDPgdrjIB13nqmkIfmLbQk1HVm-pPQtm7sXm-B2ac="
 
-pk = b64_to_bytes(b64_pub, urlsafe=True)
-sk = b64_to_bytes(b64_priv, urlsafe=True)
+def make_seed(data: bytes):
+	seed = bytearray(nacl.bindings.crypto_sign_SEEDBYTES)
+	seed[:len(data)] = data[:len(seed)]
+	seed = bytes(seed)
+	# print(len(seed) - nacl.bindings.crypto_sign_SEEDBYTES)
+	return seed
+
+seed = make_seed("alice".encode())
+
+pkSign, skSign = nacl.bindings.crypto_sign_seed_keypair(seed)
+
+# Generating a Recipient key pair for testcases
+print("PK (Ed): ", bytes_to_b58(pkSign))
+print("SK (Ed): ", bytes_to_b58(skSign))
+
+pkSeal = nacl.bindings.crypto_sign_ed25519_pk_to_curve25519(pkSign)
+skSeal = nacl.bindings.crypto_sign_ed25519_sk_to_curve25519(skSign)
+
+print("PK (Curve): ", bytes_to_b58(pkSeal))
+print("SK (Curve): ", bytes_to_b58(skSeal))
+
+# Paste in b64url data
+msg_in = "oxjRLcgYSHVOlgzBdBHyTRZtKAGWF7ZVbAnWy_ZnPSC4joMVWgMxkmPSdH1qXK3B8S1plCHSVP-quMqoLlJmB4ZErhX3HuQmGq8dBlxIHmxjVVxMQU77o4TIp1rtMR8_denZqsPAUQ=="
+
 msg_in = b64_to_bytes(msg_in, urlsafe=True)
 
-msg_out = nacl.bindings.crypto_box_seal_open(msg_in, pk, sk)
+msg_out = nacl.bindings.crypto_box_seal_open(msg_in, pkSeal, skSeal)
 
 print(msg_out.decode("ascii"))
--- aries_cloudagent/testgen.py
+++ aries_cloudagent/testgen.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from .wallet import crypto
-from .wallet.util import b64_to_bytes
+from .wallet.util import  bytes_to_b58, bytes_to_b64, b64_to_bytes, b58_to_bytes
 import nacl.bindings
 
 def make_seed(data: bytes):
@@ -11,19 +11,43 @@ def make_seed(data: bytes):
 	# print(len(seed) - nacl.bindings.crypto_sign_SEEDBYTES)
 	return seed
 
-b64_pub = "fZVwrHMKi3JynorE5eDi1jHzo-vmIFWQ9dbXfMs57jA="
-b64_priv = "q0xM-cokts_2QEqHoaiVZro2NgckC2A-20HbEFs3lnZ9lXCscwqLcnKeisTl4OLWMfOj6-YgVZD11td8yznuMA=="
+seed = make_seed("alice".encode())
 
-msg_in = """{"protected":"eyJlbmMiOiJjaGFjaGEyMHBvbHkxMzA1X2lldGYiLCJ0eXAiOiJKV00vMS4wIiwiYWxnIjoiQXV0aGNyeXB0IiwicmVjaXBpZW50cyI6W3siZW5jcnlwdGVkX2tleSI6InF2YlhZQUFyM09pWHN4QVZoWWdNd2NJTUhuVXdCLTk2ZTVNQWE4MlBOd2QybFhtRGEtQzdpMGQwdmZjTHFnRzciLCJoZWFkZXIiOnsia2lkIjoiOVRFOFIyOWRwSEx0c0poaVlGbXJCcmdTTkRpUGJWTmNvOHpIb01CRmhwaGQiLCJzZW5kZXIiOiJTLUFpQUtGeHB4RGZJYVFBREZxdlJkZno2UnZjeGh0amhSczBpWUdpVGhGZFAwdFFuNzNvcGpGelZ4ZnJDVWp5VUVMc1JiLW1VamhFY0ctVU44RXRRSlc1a0hYYTlvWmxVWUtVaG5hM0FZOUtIQmtLSmVqNXFRWmVpTU09IiwiaXYiOiJzTGFBSXRKcllwaGVMa1RydS1vRmZwRTJ4LXhwRnAwVCJ9fV19","iv":"7zNmSKQc3TJ45Pro","ciphertext":"Z9m7zNd6yQ4bkorhZjvVNKmnBhktv1FgsK8=","tag":"DSxJKQ2zgKxbS95RcN1zQg=="}""".encode("ascii")
+pkSign, skSign = nacl.bindings.crypto_sign_seed_keypair(seed)
+
+# Generating a Recipient key pair for testcases
+print("PK (Ed): ", bytes_to_b58(pkSign))
+print("SK (Ed): ", bytes_to_b58(skSign))
+
+pkSeal = nacl.bindings.crypto_sign_ed25519_pk_to_curve25519(pkSign)
+skSeal = nacl.bindings.crypto_sign_ed25519_sk_to_curve25519(skSign)
+
+print("PK (Curve): ", bytes_to_b58(pkSeal))
+print("SK (Curve): ", bytes_to_b58(skSeal))
+
+
+# Current recipient keypair used in testcase
+
+## PASTE DATA ##
+b58_pub = "EGJzjnQCJZWxww4gn9SJt6UNNrCmBkNqmCvG29VLi3pX"
+b58_priv = "5H2CB3nkxX1UiVVbE7sG6en6NXp1zPzYTzfBMU4ncCm9XMVcRrHw1anqRcGJ4xoSkpjCUVTz4jALSrVeiDC9tdyD"
+msg_in = """{"protected":"eyJlbmMiOiJjaGFjaGEyMHBvbHkxMzA1X2lldGYiLCJ0eXAiOiJKV00vMS4wIiwiYWxnIjoiQXV0aGNyeXB0IiwicmVjaXBpZW50cyI6W3siZW5jcnlwdGVkX2tleSI6Ijh0NHFWWl9LRFpSX3F3OHlCOVJwUUdyMElQeGdOSVJKR1VlNGxNRUxranljN2xibHBtdmhjWkprb3JWcm5GSV8iLCJoZWFkZXIiOnsia2lkIjoiRUdKempuUUNKWld4d3c0Z245U0p0NlVOTnJDbUJrTnFtQ3ZHMjlWTGkzcFgiLCJzZW5kZXIiOiJSOGdFRXlsSnNvLTBTSV84bExsZDBNS05HNkp2V0R3c3JrcjlMN1JfNXhYU3VrdVFsajk0RkZxU3BremR4dlVxd0RXM0U1NV9yazZBbW1Jb1NqWl90SmFKWWdVVFZ3LTlycXp0VWZ3eTBkcFRMTERNVmloQkVwYU1rMEU9IiwiaXYiOiI5cERsdjV4aWgzcmpzTlExazl2eENuZ0JFc3Y1SmZQVyJ9fV19","iv":"RzVtnhMSALLv5qsa","ciphertext":"2-NivuC-K8SvDH4_empA-fsLR8qNOPpfTNE=","tag":"pIRd32Hm8BCEW_ZYaBz2DA=="}
+"""
+## END PASTE  ##
+
+print("Using PK: ", b58_pub)
+print("Using SK: ", b58_priv)
+
+msg_in = msg_in.encode()
 
 # concat the priv and pub keys to produce the 64-byte nacl priv format
 def find_key(param):
-	sk = b64_to_bytes(b64_priv, urlsafe=True)
+	sk = b58_to_bytes(b58_priv)
 	return sk
 
 data_in, sender_VK, recip_VK = crypto.decode_pack_message(msg_in, find_key)
 
-print("data in:\n")
+print("data in:")
 
 print(data_in)
 
